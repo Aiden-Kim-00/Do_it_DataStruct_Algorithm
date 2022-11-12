024b47749d29dc7d08bbf42f896e4b8b93a47295
kodo9179 <kodi9179@gmail.com>
2022-11-12 23:01:24 +0900

    1112(2)

--- chap6_sorting/heap_sort.c
+++ chap6_sorting/heap_sort.c
@@ -1,14 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define swap(type, x, y) \
+    do                   \
+    {                    \
+        type t = x;      \
+        x = y;           \
+        y = t;           \
+    } while (0)
+
 static int *buff;
 
-
-
-void hearsort(int a[], int n)
+void heatsort(int a[], int n)
 {
-    int i;
-    
+    //초기에 전체 배열을 힙 구조로 만들기(힙 크기는 감소하지 않음=최대힙)
+    for(int i=1;i<n;i++)    
+    {
+        int c=i;
+        do{
+            int root = (c-1)/2;
+            if(a[root] < a[c])  //자신의 부모 < 자식
+            {
+                swap(int, a[root],a[c]);
+            }
+            c=root;
+        }while(c!=0);
+    } 
+    // 크기를 줄여가며 반복적으로 힙 구성
+    for(int k=n-1;k>=0;k--)    //k=10
+    {
+        swap(int, a[0],a[k]);
+        int root=0;
+        int c=1;
+        do{
+            c=2*root + 1;   //왼쪽 자식
+            if(a[c]<a[c+1] && c<k-1)
+            {
+                c++;
+            }
+            if(a[root] < a[c] && c<k)  //자신의 부모 < 자식
+            {
+                swap(int, a[root],a[c]);
+            }
+            root = c;
+        }while(c<k);
+
+    }
 }
 
 int main(void)
