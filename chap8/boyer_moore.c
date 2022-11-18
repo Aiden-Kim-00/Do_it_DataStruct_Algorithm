9b364b62014c73600d9f86da827e3261b0c855e4
kodo9179 <kodi9179@gmail.com>
2022-11-18 20:28:09 +0900

    1118_Boyer_Moore(2)

--- chap8/boyer_moore.c
+++ chap8/boyer_moore.c
@@ -17,40 +17,54 @@ int bm_sort(char txt[], char pat[])
     for(int i=0;i<length-1;i++){    //Last Letter은 제외 --> 마지막 배열 전에 Last Letter 문자가 없다면 lenght로 설정됨
         skip_table[(int)pat[i]]=length-i-1;
     }
-    for (int i = 0; i < (sizeof(skip_table)/sizeof(int)); i++)  //Skip Table 출력
-    {
-        if(skip_table[i] !=length)
-            printf("skip[%c]=%d ", (char)i, skip_table[i]);
-    }
-    printf("skip[%c]=%d\n", pat[length-1], skip_table[length-1]);
 
-
-    int now=pat_size-1; 
-
-    // do{
-    //     if(txt[now] != pat[now]){
-    //         now = 
-    //     }
-    //     else{
-    //         now--;
-    //     }
+    // for (int i = 0; i < (sizeof(skip_table)/sizeof(int)); i++)  //Skip Table 출력
+    // {
+    //     if(skip_table[i] !=length)
+    //         printf("skip[%c]=%d ", (char)i, skip_table[i]);
     // }
+    // printf("skip[%c]=%d\n", pat[length-1], skip_table[(int)pat[length-1]]);
 
 
+    // Boyer-Moore 알고리즘 수행
+    int txt_now=pat_size-1; 
+    int pat_now=pat_size-1;    
+    while(pat_now!=-1){     
+        if(txt[txt_now] != pat[pat_now]){
+            txt_now += skip_table[(int)txt[txt_now]];
+            pat_now=pat_size-1;
+            if(txt_now > txt_size)
+                return -1;
+        }
+        else{
+            txt_now--;
+            pat_now--;
+        }
+    }
+    return txt_now +1;
 }
 
 
 int main(void)
 {
     int idx;
-    char s1[20];
-    char s2[5]={'c','a','b','c'};
-    
-    bm_sort(s1,s2);
+    char txt[256];
+    char pattern[256];
 
+    puts("Boyer-Moore Algorithm");
+    printf("txt: ");
+    scanf("%s",txt);
+    printf("pattern: ");
+    scanf("%s",pattern);
 
+    int result = bm_sort(txt,pattern);
+    if(result == -1)
+    {
+        printf("patter is no exist in txt\n");
+    }
+    else{
+        printf("search position: %d\n", result);
+    }
 
-
-    
     return 0;
 } 
